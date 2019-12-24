e50020b6768ff4c59be057dc50dff463afda2c7d
AkSelll <44325685+AkSelll@users.noreply.github.com>
2019-12-24 17:01:26 +0200

    Add project and some mechanics

---
+++ 2D Work/Assets/Scripts/CloudMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour
+{
+    public GameObject cloud;
+    public BoxCollider2D BackgroundColl;
+    public float speed;
+    Vector2 cloudMov;
+    float r_bound;
+    float l_bound;
+    void Start()
+    {
+        cloudMov = cloud.transform.position;
+    }
+
+    void Update()
+    {
+        r_bound = BackgroundColl.bounds.center.x + BackgroundColl.bounds.extents.x;
+        l_bound = BackgroundColl.bounds.center.x - BackgroundColl.bounds.extents.x;
+        /*if (cloud.transform.position.x < r_bound+3f)
+        {
+            cloudMov += Vector2.right * speed * Time.deltaTime;
+            cloud.transform.position = cloudMov;
+        }
+        else if (cloud.transform.position.x >= r_bound+3f)
+        {
+            cloudMov = new Vector3(l_bound - 3f, cloud.transform.position.y, cloud.transform.position.z);
+            cloud.transform.position = cloudMov;
+        }*/
+        if (cloud.transform.position.x > l_bound-3f)
+        {
+            cloudMov += Vector2.right * speed * Time.deltaTime;
+            cloud.transform.position = cloudMov;
+        }
+        else if (cloud.transform.position.x <= l_bound-3f)
+        {
+            cloudMov = new Vector3(r_bound + 3f, cloud.transform.position.y, cloud.transform.position.z);
+            cloud.transform.position = cloudMov;
+        }
+    }
+}
---
+++ 2D Work/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    Vector2 movement = Vector2.zero;
+    public float speed = 50f;
+    bool facing = true;
+    public Animator animator;
+    public Joystick joystick;
+    void Update()
+    {
+        movement.x = joystick.Horizontal;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", 0f);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(movement.x > 0 && !facing)
+        {
+            Flip();
+        }
+        if(movement.x < 0 && facing)
+        {
+            Flip();
+        }
+
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        facing = !facing;
+        Vector3 xMirror = transform.localScale;
+        xMirror.x *= -1;
+        transform.localScale = xMirror;
+    }
+}
